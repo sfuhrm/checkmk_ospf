a7064aaf6b279b97345dc9aa5f2c6d3d24aed4a2
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2024-02-05 15:59:05 +0100

    checkmk_isis devcontainer

---
+++ .devcontainer/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAME=$(python -c 'print(eval(open("package").read())["name"])')
+rm /omd/sites/cmk/var/check_mk/packages/* ||:
+ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
+
+mkp -v pack $NAME
+
+# Set Outputs for GitHub Workflow steps
+if [ -n "$GITHUB_WORKSPACE" ]; then
+    echo "pkgfile=$(ls *.mkp)" >> $GITHUB_OUTPUT
+    echo "pkgname=${NAME}" >> $GITHUB_OUTPUT
+    VERSION=$(python -c 'print(eval(open("package").read())["version"])')
+    echo "pkgversion=$VERSION"  >> $GITHUB_OUTPUT
+fi
